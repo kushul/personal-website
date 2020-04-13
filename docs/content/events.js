6491c2ac3bd87a609f1a83c71e017d859b0b9e33
Kushul Soomaree <kushul@lsl.digital>
2020-04-14 03:01:24 +0400

    generated using npm run deploy-gh

---
+++ docs/content/events.js
@@ -0,0 +1,21 @@
+const events = [
+    {
+        type: 'Participation',
+        color: '#00bcd4',
+        title: '1st place in the krathathon 2019',
+        image:
+            'https://lh3.googleusercontent.com/proxy/1tydbA714yRi4QfNDIUW-4f0BDEFepDCmJKCsTAPV-pSWdatP0hEgAkb-ya2ZQxfx8GC0vy5kMisUL4Jg7DkaMtlh1-KrzT_Jxn7ElkEWVmhCmQoTNgX9dJSg3nNDy8Pnnog-FE6lCBGbfg3GEz-D6gBWSZGKf6owGFhBfkV4AxiLQ80zZFalw',
+        date: 'test',
+        name: 'Krakathon 2019',
+        country: 'Mauritius',
+        links: [
+            {
+                text: 'Report by ict.io',
+                url:
+                    'https://www.growthbunker.dev/vuetimeline/?ref=madewithvuejs.com'
+            }
+        ]
+    }
+]
+
+export default events;
---
+++ docs/content/projects.js
@@ -0,0 +1,57 @@
+const projects = [
+    {
+        title: 'COVID-19 Mauritius | LSL Digital',
+        url: 'https://www.lexpress.mu',
+        role: 'Front End developer',
+        tech: ['TailwindCSS', 'VueJS', 'Github Actions'],
+        description: 'A page focussed on the statistics of COVID-19 in Mauritius',
+        featured: true,
+        thumbnail: 'lsl.png',
+        date: '2017-02-26',
+        company: 'Open Source',
+        color: 'turquoise',
+        icon: 'code'
+    },
+    {
+        title: 'COVID-19 Mauritius | LSL Digital',
+        url: 'https://www.lexpress.mu',
+        role: 'Front End developer',
+        tech: ['TailwindCSS', 'VueJS', 'Github Actions'],
+        description: 'A page focussed on the statistics of COVID-19 in Mauritius',
+        featured: true,
+        thumbnail: 'lsl.png',
+        date: '2017-02-26',
+        company: 'Open Source',
+        color: 'turquoise',
+        icon: 'code'
+    },
+    {
+        title: 'COVID-19 Mauritius | LSL Digital',
+        url: 'https://www.lexpress.mu',
+        role: 'Front End developer',
+        tech: ['TailwindCSS', 'VueJS', 'Github Actions'],
+        description: 'A page focussed on the statistics of COVID-19 in Mauritius',
+        featured: true,
+        thumbnail: 'lsl.png',
+        date: '2017-02-26',
+        company: 'Open Source',
+        color: 'turquoise',
+        icon: 'code'
+    },
+    {
+        title: 'COVID-19 Mauritius | LSL Digital',
+        url: 'https://www.lexpress.mu',
+        role: 'Front End developer',
+        tech: ['TailwindCSS', 'VueJS', 'Github Actions'],
+        description: 'A page focussed on the statistics of COVID-19 in Mauritius',
+        featured: true,
+        thumbnail: 'lsl.png',
+        date: '2017-02-26',
+        company: 'Open Source',
+        color: 'turquoise',
+        icon: 'code'
+    },
+
+]
+
+export default projects;
