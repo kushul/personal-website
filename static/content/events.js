70d8d44084e01256f3a19a30bfb107591bed3596
Kushul Soomaree <kushul@lsl.digital>
2020-07-03 17:18:45 +0400

    more events added

--- static/content/events.js
+++ static/content/events.js
@@ -8,7 +8,7 @@ const events = [
         bg_position: "bg-bottom",
         date: '2020-02-20T00:00:00.000Z',
         name: 'Vuejs conference',
-        country: 'Amsterdam',
+        country: 'Theater, Amsterdam',
         icon: "attendee.svg",
         links: [
             {
@@ -29,7 +29,7 @@ const events = [
         bg_position: "bg-top",
         date: '2019-12-07T00:00:00.000Z',
         name: 'Webcup Final',
-        country: 'Mayotte',
+        country: 'Mamoudzou, Mayotte',
         icon: "competition.svg",
         links: [
             {
@@ -49,12 +49,31 @@ const events = [
         date: '2019-10-19T00:00:00.000Z',
         name: 'Krakathon',
         icon: "competition.svg",
-        country: 'Mauritius',
+        country: 'Caudan Art Center, Mauritius',
         links: [
             {
                 text: 'Report by ict.io',
                 url:
                     'https://ict.io/krakathon-2019-team-kalchul-devance-ses-adversaires-de-4000-points/'
+            }
+        ]
+    },
+    {
+        type: 'speaker',
+        color: 'bg-green-700',
+        title: 'Talking about UI code with Namespaces',
+        image:
+            'namespaces-2019.jpg',
+        bg_position: "bg-center",
+        date: '2019-06-22T00:00:00.000Z',
+        name: 'Front End Meetup',
+        country: 'Moka, Mauritius',
+        icon: "speaker.svg",
+        links: [
+            {
+                text: 'Tweet By Sandeep',
+                url:
+                    'https://twitter.com/__Sun__/status/1142320655012155392?s=19'
             }
         ]
     },
@@ -67,13 +86,70 @@ const events = [
         bg_position: "bg-center",
         date: '2019-06-07T00:00:00.000Z',
         name: 'Switch conference',
-        country: 'Mauritius',
+        country: 'Jinfei, Mauritius',
         icon: "attendee.svg",
         links: [
             {
                 text: 'Report by mscc.mu',
                 url:
                     'https://www.mscc.mu/untitled-2/'
+            }
+        ]
+    },
+    {
+        type: 'speaker',
+        color: 'bg-yellow-700',
+        title: 'Talking about Vuex in Devcon 2019',
+        image:
+            'devcon-2019.jpeg',
+        bg_position: "bg-center",
+        date: '2019-04-12T00:00:00.000Z',
+        name: 'Developer Conference',
+        icon: "speaker.svg",
+        country: 'Voila hotel, Mauritius',
+        links: [
+            {
+                text: 'Call for speakers',
+                url:
+                    'https://sessionize.com/devcon-mauritius-2019'
+            }
+        ]
+    },
+    {
+        type: 'competition',
+        color: 'bg-yellow-700',
+        title: '2nd place in krakathon 2018',
+        image:
+            'wachawi-2nd-prize_2019.jpg',
+        bg_position: "bg-center",
+        date: '2018-10-20T00:00:00.000Z',
+        name: 'Krakathon',
+        icon: "competition.svg",
+        country: 'Turbine, Mauritius',
+        links: [
+            {
+                text: 'Blogpost By Ish',
+                url:
+                    'https://www.lsl.digital/blog/wachawi-by-lsl-digital-at-the-krakathon-2018'
+            }
+        ]
+    },
+    {
+        type: 'speaker',
+        color: 'bg-yellow-700',
+        title: 'Talking about CSS grid in Devcon 2018',
+        image:
+            'devcon-2018.jpg',
+        bg_position: "bg-center",
+        date: '2018-05-17T00:00:00.000Z',
+        name: 'Developer Conference',
+        icon: "speaker.svg",
+        country: 'Voila hotel, Mauritius',
+        links: [
+            {
+                text: 'FB post by MSCC',
+                url:
+                    'https://www.facebook.com/MauritiusSoftwareCraftsmanshipCommunity/photos/discover-css-grid-by-kushul-soomaree-jordan-bienvenue-starting-in-5-minutes-in-e/981006642058399/'
             }
         ]
     },
@@ -87,7 +163,7 @@ const events = [
         date: '2018-05-06T00:00:00.000Z',
         name: 'Webcup Maurice',
         icon: "competition.svg",
-        country: 'Mauritius',
+        country: 'Ebene tower, Mauritius',
         links: [
             {
                 text: 'Article by lexpress.mu',
